b36371c2743c2f59ad311a95c68caf4871c7a337
adametna <adam.salama2002@gmail.com>
2023-01-08 23:09:31 +0100

    update

--- git/components/product_card.tsx
+++ git/components/product_card.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import styles from "../styles/Card.module.scss";
+import Router from "next/router";
 
 
-function Card(props) {
+export default function Card({props}: any) {
+  const redirect=() => {
+    localStorage.setItem('clickId', props._id)
+    Router.push("/Product_page")
+  }
   return (
-    <div className={styles.card}>
+    <div className={styles.card} onClick={redirect}>
       <img className={styles.img}
-        src={props.img}
-        alt="PSG Home Shirt"
+        src={props.image}
+        alt="Shirt"
       ></img>
-      <h1>{props.title}</h1>
-      <p className={styles.price}>{props.price}</p>
-      <p>
-        <button>Add to Cart</button>
-      </p>
+      <h1>{props.name}</h1>
+      <p className={styles.price}>{props.price}.00€</p>
     </div>
   );
 }
 
-export default Card;
--- git/pages/Premier-League.tsx
+++ git/pages/Premier-League.tsx
@@ -1,17 +1,49 @@
-import React from 'react';
-import Navigation from './Navigation';
-import styles from '../styles/Products.module.scss'
+import React, {useState, useEffect} from "react";
+import Navigation from "./Navigation";
+import styles from "../styles/Products.module.scss";
+import Card from "../components/product_card";
+import { getProduct } from "../api/getProduct";
 
 const PremierLeague = () => {
+    const  [product, setProduct]: any= useState();
+  useEffect(()=>{
+getProduct().then((res)=>{
+setProduct(res.data)
+
+
+})
+  }, [])
+
     return (
-        <div className={styles.header}>
+        <div>
             <Navigation/>
+        <div className={styles.header}>
             <h1>PREMIER LEAGUE</h1>
             <div>
             maillots tah les fous
             </div>
             
         </div>
+        <div className={styles.body}>
+      <div className={styles.cards}>
+        {product?.filter((product: any, index: any)=> {
+          if (product.categoryId==="Premier League") {
+            return product;
+          }
+        })
+        .map((product: any, index: any)=> {
+          return (
+            <Card props={product}/>
+          ) 
+          
+        })}
+        
+      </div>
+      </div>
+        
+        </div>
+        
+        
     );
 };
 
